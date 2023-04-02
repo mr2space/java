d88eddd30f5bf761af634974605caa9b8e6b9d79
mr2space <princegoswami.space@gmail.com>
2023-04-03 00:09:47 +0530

    first commit

---
+++ code/Hello.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String args[]){
+        char num1 = '3';
+        int num2 = 23;
+        int result = (int)num1 ;
+        System.out.println(result);
+    }
+}
